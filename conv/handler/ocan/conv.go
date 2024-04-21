c7a989e3c9a9b6dddeddb7b470390a4f98ddf204
yuanhangjun <437132604@qq.com>
2024-04-21 11:58:13 +0800

    feat：更新了巨量的sdk版本，删除了巨量事件宏

--- conv/handler/ocan/conv.go
+++ conv/handler/ocan/conv.go
@@ -9,7 +9,6 @@ import (
 	"time"
 
 	oceancore "github.com/bububa/oceanengine/marketing-api/core"
-	oceanenum "github.com/bububa/oceanengine/marketing-api/enum"
 	oceanreq "github.com/bububa/oceanengine/marketing-api/model/conversion"
 )
 
@@ -68,36 +67,19 @@ func (h Handler) Validate(req *types.ConvReq) error {
 	if req.ClickID == "" {
 		return fmt.Errorf("clickid is nil")
 	}
+	// NOTE: event
+	if req.ConvEvent == "" {
+		return fmt.Errorf("conv_event is nil")
+	}
 
 	return nil
 }
 
 // MakeReq 构造请求参数
 func (h Handler) MakeReq(req *types.ConvReq) *HandlerReq {
-	// step1. 事件转化
-	var event oceanenum.ConversionEventType
-
-	switch req.ConvEvent {
-	case oceanenum.Conversion_ACTIVE:
-		event = oceanenum.Conversion_ACTIVE
-	case oceanenum.Conversion_CUSTOMER_EFFECTIVE:
-		event = oceanenum.Conversion_CUSTOMER_EFFECTIVE
-	case oceanenum.Conversion_PAGE_VIEW:
-		event = oceanenum.Conversion_PAGE_VIEW
-	case oceanenum.Conversion_CLUE_CONFIRM:
-		event = oceanenum.Conversion_CLUE_CONFIRM
-	case "submit_certification": // 提交认证 包里没有预定义宏，这里直接根据文档字段硬编码
-		event = "submit_certification"
-	case oceanenum.Conversion_ACTIVE_REGISTER:
-		event = oceanenum.Conversion_ACTIVE_REGISTER
-	case oceanenum.Conversion_GAME_ADDICTION:
-		event = oceanenum.Conversion_GAME_ADDICTION
-	default:
-		event = oceanenum.Conversion_FORM
-	}
-	// step2. 构造SDK请求参数
+	// step1. 构造SDK请求参数
 	conversionReq := &oceanreq.Request{
-		EventType: event,
+		EventType: req.ConvEvent,
 		Context:   &oceanreq.Context{Ad: &oceanreq.ContextAd{Callback: req.ClickID}},
 		Timestamp: time.Now().UnixMilli(),
 	}
--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/halo2024/sugar
 go 1.21.6
 
 require (
-	github.com/bububa/oceanengine v1.24.2
+	github.com/bububa/oceanengine v1.24.8
 	github.com/fatih/structs v1.1.0
 )
